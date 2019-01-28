fb97ff79be54353b33b6448989448b23ea9e3b01
Nguyễn Thành <thanhnguyen2187@gmail.com>
2019-01-28 18:18:46 +0700

    Temporarily done with login form.

---
+++ HotelManagement/LoginForm.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace HotelManagement
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.UsernameLabel = new System.Windows.Forms.Label();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.UsernameTextbox = new System.Windows.Forms.TextBox();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.KeyImage = new System.Windows.Forms.PictureBox();
+            this.PasswordTextbox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.KeyImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UsernameLabel
+            // 
+            this.UsernameLabel.AutoSize = true;
+            this.UsernameLabel.Location = new System.Drawing.Point(16, 53);
+            this.UsernameLabel.Name = "UsernameLabel";
+            this.UsernameLabel.Size = new System.Drawing.Size(81, 13);
+            this.UsernameLabel.TabIndex = 0;
+            this.UsernameLabel.Text = "Tên đăng nhập";
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Location = new System.Drawing.Point(16, 79);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(52, 13);
+            this.PasswordLabel.TabIndex = 0;
+            this.PasswordLabel.Text = "Mật khẩu";
+            // 
+            // UsernameTextbox
+            // 
+            this.UsernameTextbox.Location = new System.Drawing.Point(104, 50);
+            this.UsernameTextbox.Name = "UsernameTextbox";
+            this.UsernameTextbox.Size = new System.Drawing.Size(100, 20);
+            this.UsernameTextbox.TabIndex = 1;
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(48, 102);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(75, 23);
+            this.LoginButton.TabIndex = 3;
+            this.LoginButton.Text = "Đăng nhập";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(129, 102);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelButton.TabIndex = 4;
+            this.CancelButton.Text = "Hủy bỏ";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLabel.Location = new System.Drawing.Point(44, 16);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(216, 20);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "Phần mềm quản lý khách sạn";
+            // 
+            // KeyImage
+            // 
+            this.KeyImage.Image = ((System.Drawing.Image)(resources.GetObject("KeyImage.Image")));
+            this.KeyImage.Location = new System.Drawing.Point(210, 50);
+            this.KeyImage.Name = "KeyImage";
+            this.KeyImage.Size = new System.Drawing.Size(74, 74);
+            this.KeyImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.KeyImage.TabIndex = 7;
+            this.KeyImage.TabStop = false;
+            // 
+            // PasswordTextbox
+            // 
+            this.PasswordTextbox.Location = new System.Drawing.Point(104, 76);
+            this.PasswordTextbox.Name = "PasswordTextbox";
+            this.PasswordTextbox.PasswordChar = '⏺';
+            this.PasswordTextbox.Size = new System.Drawing.Size(100, 20);
+            this.PasswordTextbox.TabIndex = 2;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 139);
+            this.Controls.Add(this.PasswordTextbox);
+            this.Controls.Add(this.KeyImage);
+            this.Controls.Add(this.TitleLabel);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.UsernameTextbox);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.UsernameLabel);
+            this.Name = "LoginForm";
+            this.Text = "Khung đăng nhập";
+            ((System.ComponentModel.ISupportInitialize)(this.KeyImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label UsernameLabel;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.TextBox UsernameTextbox;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.PictureBox KeyImage;
+        private System.Windows.Forms.TextBox PasswordTextbox;
+    }
+}
---
+++ HotelManagement/LoginForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HotelManagement
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            if(UsernameTextbox.Text.Equals("admin")
+                && PasswordTextbox.Text.Equals("admin"))
+            {
+                Hide();
+                MainForm form = new MainForm();
+                form.FormClosed += (s, args) => Close();
+                form.Show();
+            }
+        }
+    }
+}
